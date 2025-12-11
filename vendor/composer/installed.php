a01bdb6f986dda066c8af20700f4349d31c76db8
srtfisher <srtfisher@users.noreply.github.com>
2025-12-11 16:34:20 +0000

    Built changes from develop c8432e01e954710126c409ab88d2c36096fb4694

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/meta-inspector',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'c8432e01e954710126c409ab88d2c36096fb4694',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/meta-inspector' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'c8432e01e954710126c409ab88d2c36096fb4694',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
